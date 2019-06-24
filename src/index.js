c953aa9f4c856005b22fec3976958f34cd7b8d08
troyclarke69 <38792525+troyclarke69@users.noreply.github.com>
2019-06-24 11:32:45 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ import { createStore } from 'redux'
 import rootReducer from './reducers'
 import App from './components/App'
 
+import Fetch from './components/fetch'
+
 const store = createStore(rootReducer)
 
 render(
   <Provider store={store}>
     <App />
+	  <Fetch  />
   </Provider>,
+  
+
+  
   document.getElementById('root')
 )
